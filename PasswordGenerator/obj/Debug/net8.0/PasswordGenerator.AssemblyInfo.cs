35161565facb4e991f22e917173973b2a8eadd60
Matheus <matheusramossilva2003@gmail.com>
2025-02-21 02:10:42 -0300

    upload

--- PasswordGenerator/obj/Debug/net8.0/PasswordGenerator.AssemblyInfo.cs
+++ PasswordGenerator/obj/Debug/net8.0/PasswordGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9052a62816b0f9fbda746f8cc8649b08db46d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
